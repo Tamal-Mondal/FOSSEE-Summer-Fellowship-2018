b333211c75384496c22aa26a1e8d7291ddb0f583
Tamal Mondal <tamalmondal495@gmail.com>
2018-04-06 18:42:37 +0530

    Final Update

--- foss/views.py
+++ foss/views.py
@@ -10,7 +10,7 @@ def index(req):
 		try:
 			objs=t.objects.filter(act_sub_date__year=req.POST['year'],act_sub_date__month=req.POST['month'])
 			for i in objs:
-				dates.append({'cname':i.tutorial_name,'expt_date':i.exp_sub_date,'act_date':i.act_sub_date})
+				dates.append({'cname':i.tutorial_name,'fn':i.foss.name,'expt_date':i.exp_sub_date,'act_date':i.act_sub_date})
 			objs=objs.distinct().values('publisher').annotate(c=Count('publisher')).order_by('publisher')
 			usr_table=[{'count':i['c'],'pub':user.objects.filter(pk=i['publisher'])[0].name} for i in objs]
 			return render(req,'index.html',{'usr_table':usr_table,'dates':dates})
